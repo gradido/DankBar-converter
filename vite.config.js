35fd5059869438c3d527fdcb57b20716d6e6b2ed
ogerly <fridolin@tutanota.com>
2023-05-06 11:43:29 +0200

    change vite config: base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/dankbar-converter/",
+  base: "/DankBar-converter/",
   build: {
     assetsDir: 'assets'
   }
